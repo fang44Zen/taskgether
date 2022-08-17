80a5228986c50df0a1f6f5f948e19808fb171458
François Lyons <lyons.fr55@gmail.com>
2022-08-17 17:40:46 +0200

    fonction coché/décoché task terminé ou non, fonction du bouton modifier en cour

--- src/component/task-creator/task-creator.js
+++ src/component/task-creator/task-creator.js
@@ -1,13 +1,39 @@
+import { useState } from 'react';
 import './task-creator.css';
 
-const TaskCreator = ({taskText, clickDelete, taskId})=>{
-    return(
-        <div className="list-style">
-            <p>{taskText}</p>
-            <button >modify</button>
-            <button onClick={clickDelete}>delete</button>
+const TaskCreator = ({taskText, clickDelete, onCheck, styleText, clickAcceptMod})=>{
+    const [isOnModifying, setOnModifying] = useState(false);
+
+    const clickModify = () =>{
+        setOnModifying(true)
+    }
+
+    const clickCancelMod = () =>{
+        setOnModifying(false)
+    }
+
+    
+
+    if(!isOnModifying){
+        return(
+        
+            <div className="list-style" >
+                <input type="checkbox" onClick={onCheck} />
+                <p className={styleText}>{taskText}</p>
+                <button onClick={clickModify}>modify</button>
+                <button onClick={clickDelete}>delete</button>
+            </div>
+        )
+    }else{
+        return(
+        <div className="list-style" >
+            <input type="text" value={taskText} className='text-list-style'/>
+            <button onClick={clickAcceptMod}>accept</button>
+            <button onClick={clickCancelMod}>cancel</button>
         </div>
-    )
+        )
+}
+
 }
 
 export default TaskCreator;
--- src/component/task-zone/task-zone.js
+++ src/component/task-zone/task-zone.js
@@ -1,7 +1,8 @@
-import { doc, onSnapshot,  collection, deleteDoc} from "firebase/firestore";
+import { doc, onSnapshot,  collection, deleteDoc, updateDoc} from "firebase/firestore";
 import TaskCreator from "../task-creator/task-creator";
 import { db } from "../../firebase/firebase-config";
 import {  useEffect, useState } from "react";
+import '../task-creator/task-creator.css';
 
 const TaskZone =  () =>{
     const [taskText, setTaskText] = useState([]);
@@ -13,15 +14,38 @@ const TaskZone =  () =>{
                 task: doc.data(),
             })))
         ), []);
+    
+    const setCheck= async ( e, id)=>{
+       const docRef = doc(db, "tasks", id);
+       if(e.target.checked){
+        await updateDoc(docRef, {
+            isCompleted: true
+           });
+       }else{
+        await updateDoc(docRef, {
+            isCompleted: false
+           });
+       }
+       
+    }
 
+    const delTask =async (id)=>{
+        await deleteDoc(doc(db, "tasks", id))
+    }
+    
     return(
         <div className="taskzone">
             
             {taskText.map((task)=>(
+                
                 <li key={task.id}>
-                    <TaskCreator taskText={task.task.taskName} clickDelete={ () => deleteDoc(doc(db, "tasks", task.id))}/>
-               </li>
-        ) )}
+                    <TaskCreator 
+                    taskText={task.task.taskName} 
+                    styleText={task.task.isCompleted ? 'task-done': 'text-list-style'}
+                    clickDelete={() => delTask(task.id)}
+                    onCheck={(e) =>setCheck(e,task.id)}/>
+               </li> 
+               ) )}
         
         </div>
     )
