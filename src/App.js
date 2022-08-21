8cf04102c9b7387fed1b40181cda235029bf7179
François Lyons <lyons.fr55@gmail.com>
2022-08-21 22:33:29 +0200

    sign in en cour de creation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TodoZone from './component/todo-zone/todo-zone.component';
+import SignIn  from './component/sign-in/sign-in.component';
 import NavigationBar from './component/navigation-bar/navigation-bar';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <NavigationBar />
         <Routes>
           <Route path="/todo-app" element={<TodoZone />} />
+          <Route path="/sign-in" element={<SignIn />} />
         </Routes>
       </BrowserRouter>
     </div>
