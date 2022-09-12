cff4437e210adf20d67ce9ad20c3b484214e26d7
François Lyons <lyons.fr55@gmail.com>
2022-09-12 20:16:31 +0200

    faut avancer un peux fréro git add .

---
+++ src/component/sign-up/sign-up.component.jsx
@@ -0,0 +1,15 @@
+
+
+const SignUp = () =>{
+    return(
+        <div>
+            <h3>Create Account</h3>
+            <label>
+                Nom:
+            </label>
+            <input></input>
+        </div>
+    )
+}
+
+export default SignUp;
