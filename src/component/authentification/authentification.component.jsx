c4cd9b56bd09a53d7dbc20b381310b19a8c4a224
François Lyons <lyons.fr55@gmail.com>
2022-10-06 23:21:33 +0200

    création de compte <fonctionnelle>

--- src/component/authentification/authentification.component.jsx
+++ src/component/authentification/authentification.component.jsx
@@ -1,8 +1,42 @@
 
 import './authentification.styles.scss';
 import { FcGoogle } from 'react-icons/fc';
+import { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {auth} from'../../firebase/firebase-config';
+
 
  const Authentification = () =>{
+    const [emailUser, setEmailUser] = useState("");
+    const [passUser, setPassUser] = useState("");
+
+    const readEmail = (e) => {
+        const email = e.target.value;
+        setEmailUser(email);
+    }
+    const readPass = (e) => {
+        const pass = e.target.value;
+        setPassUser(pass);
+    }
+
+    const createAccountButton = () =>{
+        createUserWithEmailAndPassword(auth, emailUser, passUser)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+          });
+        
+        console.log(emailUser);
+        console.log(passUser);
+    }
+
+    
     return(
         <div className='authentification-main'>
             <div className="connect-account">
@@ -31,28 +65,28 @@ import { FcGoogle } from 'react-icons/fc';
                 
                 <form className='form-auth'>
                     <div className='form-block'>
-                        <input type="text" className='form-block__input' required/>
+                        <input type="text" className='form-block__input' required onChange={readEmail} value={emailUser} />
                         <label className='form-block__label'>
                             <span class="material-symbols-outlined auth-icon">mail</span>
                             Email
                         </label>
                     </div>
                     <div className='form-block'>
-                        <input type="text"className='form-block__input' required/>
+                        <input type="password" className='form-block__input' required value={passUser} onChange={readPass}/>
                         <label className='form-block__label'>
                             <span class="material-symbols-outlined auth-icon">lock</span>
                             Password
                         </label>
                     </div>
                     <div className='form-block'>
-                        <input type="text"className='form-block__input' required/>
+                        <input type="password"className='form-block__input' required/>
                         <label className='form-block__label'>
                             <span class="material-symbols-outlined auth-icon">lock</span>
                             Confirm Password
                         </label>
                     </div>
                     <div className='buttons-create-account'>
-                        <button className='buttons-create-account__bca'>Create Acount</button>
+                        <button className='buttons-create-account__bca' onClick={createAccountButton}>Create Acount</button>
                         <button className='buttons-create-account__bCAWG'><FcGoogle />Connect with google</button>
                     </div>
                     
